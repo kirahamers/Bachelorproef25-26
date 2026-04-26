7b0c330b479398b21a303102d326c22bd131510a
kirahamers <kira.hamers@student.hogent.be>
2026-04-26 15:55:44 +0200

    aanpassing web scraping

--- ScradaKYC.Api/obj/Debug/net9.0/ScradaKYC.Api.AssemblyInfo.cs
+++ ScradaKYC.Api/obj/Debug/net9.0/ScradaKYC.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScradaKYC.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a74e4555d3a4f369a6ae5be60fb40c1ac12b2edf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9317e76e0b9c2373f1ab9cfed03bff3071c5a3b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScradaKYC.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScradaKYC.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
